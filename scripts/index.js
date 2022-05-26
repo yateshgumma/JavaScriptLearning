600f1e6c22090a1dc3eb0e88c35b253ba40e862b
yateshgumma <yateshy38@gmail.com>
2022-05-25 23:32:17 -0400

    Variables-Constants-Operatos

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+let userName = "MAX";
+const totalUsers = 15;
+console.log(userName);
+console.log(totalUsers);
+userName = "Jhon";
+console.log(userName);
+totalUsers = 20;
+console.log(totalUsers);
+
+
+let currentResult = 0;
+currentResult = currentResult+10;
+console.log(currentResult);
